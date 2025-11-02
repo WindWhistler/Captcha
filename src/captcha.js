fb187d66918fcf3389ac0a55d739fc5ec0870d7c
Wind Whistler <nameemaan@gmail.com>
2025-11-02 22:08:11 +0300

    feat: Заработало! (в режиме сбора данных)
    TODO: Допилить красивостей на ввод

---
+++ src/captcha.js
@@ -0,0 +1,125 @@
+async function getCaptcha() {
+  console.log("Hi anon!");
+  do {
+    t=1+getRandomInt(16000);
+    currentCaptcha = await getNewCaptcha(t);
+  } while (getPngDimensions(currentCaptcha.fg).width < 275)
+  document.getElementById("input").value = "";
+
+  console.log(getPngDimensions(currentCaptcha.fg).width);
+  console.log(getPngDimensions(currentCaptcha.bg).width);
+
+  //Set captcha
+  let fg = document.getElementById("fg");
+  let bg = document.getElementById("bg");
+  let outer = document.getElementById("outer");
+  let form = document.getElementById("form");
+  bg.setAttribute("style",`
+    overflow:auto; 
+    background-position: 0px top; 
+    width: ${getPngDimensions(currentCaptcha.bg).width}px; 
+    height: 100%; 
+    position: absolute;
+    background-repeat: no-repeat;
+    background-image: url("data:image/png;base64,` + currentCaptcha.bg + `");`
+  );
+  fg.setAttribute("style",`
+    background-position: 0px top; 
+    width: ${getPngDimensions(currentCaptcha.fg).width}px; 
+    height: 81px; 
+    position: fixed;
+    background-repeat: no-repeat; 
+    background-image: url("data:image/png;base64,` + currentCaptcha.fg + `");`
+  );
+  outer.setAttribute("style",`
+    width: ${Math.max(275,getPngDimensions(currentCaptcha.fg).width)}px;  
+    height: 96px; 
+    position: relative; 
+    display: flex; 
+    overflow:auto;`
+  );
+  form.setAttribute("style",`
+    position: relative;
+    width: ${Math.max(275,getPngDimensions(currentCaptcha.fg).width)}px;
+    background: #eee;
+    border: 1px solid #777; 
+    margin: 2px 0 2px 0;
+    vertical-align: middle;"`
+  );
+  mode = currentCaptcha.mode;
+  answer = currentCaptcha.answer;
+  
+  let post = document.getElementById("post");
+  post.disabled = false;
+  rep = document.getElementById("reply");
+  rep.hidden = true;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+async function getNewCaptcha(index) {
+  var res = await fetch(this.location.origin + "/captcha/" + index);
+  return await res.json();
+}
+
+async function checkCaptcha() {
+  let post = document.getElementById("post");
+  post.disabled = true;
+  
+  let fg = document.getElementById("fg");
+  let bg = document.getElementById("bg");
+  bg.setAttribute("style",`
+    overflow:auto; 
+    background-position: 0px top; 
+    width: 100%; 
+    height: 100%; 
+    position: absolute;
+    background-repeat: no-repeat;
+    background-image: ""`
+  );
+  fg.setAttribute("style",`
+    background-position: 0px top; 
+    width: 100%; 
+    height: 81px; 
+    position: fixed;
+    background-repeat: no-repeat; 
+    background-image: ""`
+  );
+
+  testAnswer = document.getElementById("input").value;
+  document.getElementById("input").value = "";
+  if (mode == 'dev') {
+    rep = document.getElementById("reply");
+    rep.hidden = false;
+    res = await fetch(this.location.origin = "/captcha/" + t, {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        bg:currentCaptcha.bg,
+        fg:currentCaptcha.fg,
+        answer:testAnswer
+      })
+    });
+  }
+  else {
+    //Проверять ответ, делать красивости
+    //res = await fetch()
+  }
+}
+
+
+function getPngDimensions(base64) {
+  const header = atob(base64.slice(0, 50)).slice(16,24)
+  const uint8 = Uint8Array.from(header, c => c.charCodeAt(0))
+  const dataView = new DataView(uint8.buffer)
+
+  return {
+    width: dataView.getInt32(0),
+    height: dataView.getInt32(4)
+  }
+}
--- src/express.js
+++ src/express.js
@@ -1,19 +1,54 @@
+const { stringify } = require('csv-stringify/sync');
 const express = require('express');
 const app = express();
 const port = 3000;
 
+csv = require('csv-parser');
+csv_stringify = require('csv-stringify');
+fs = require('fs');
+captchaArray = [];
+fs.createReadStream('src/img/raw_base64_dataset.csv')
+  .pipe(csv())
+  .on('data', (data) => {captchaArray.push(data);})
+  .on('end', () => {console.log("Loaded Captcha!"/*captchaArray*/);});
+
 app.use(express.static(__dirname + '/'));
 
-app.get('/', (req, res) => {
-  //res.send('Hello World!');
-  res.sendFile(__dirname+'/index.html');
-})
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   //res.send('Hello World!');
+//   res.send({'s':req});
+//   res.sendFile(__dirname+'/index.html');
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 app.get('/img/:imgName', (req, res) => {
-  //res.send('Hello World!');
+  const re_fg = /fg(\d+)\.png/;
+  const re_bg = /bg(\d+)\.png/;
+  if ((r = re_fg.exec(imgName)) !== null) {
+    captchaArray[r[1]]
+  }
   res.sendFile(__dirname+'/img/'+imgName);
+});
+
+app.get('/captcha/:Index', (req, res) => {
+  let r = captchaArray[req.params["Index"]];
+  r.mode = "use"
+  if (!r.answer) {
+    r.answer = "";
+    r.mode = "dev";
+  }
+  res.send(r);
+});
+
+app.post('/captcha/:Index', (req, res) => {
+  let index = req.params["Index"];
+  let body = req.body;
+  let data = `\n${index},${body.bg},${body.fg},${body.answer}`;
+  fs.appendFileSync('src/data/dataset.csv', data);
+  console.log("Appended successfully?");
 })
